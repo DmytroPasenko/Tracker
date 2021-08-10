ae7ab494da144247910204d4fe4ad2b9d3a6bb06
DmytroPasenko <elengardway@gmail.com>
2021-08-10 11:33:22 +0300

    DeleteMember function is done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 /*add-member*/
-const members = [];
-const deleteButtons = [];
+let members = {},
+  memberIndex = 0,
+  deleteButtons = {};
 const membersBlock = document.getElementById("membersBlock");
 
 function addMember() {
-  members.push(
-    document.getElementById("memberName").value +
-      "&" +
-      document.getElementById("memberInitiative").value
-  );
+  // members.push(
+  //   document.getElementById("memberName").value +
+  //     "&" +
+  //     document.getElementById("memberInitiative").value
+  // );
+
+  members[memberIndex] = {
+    index: memberIndex,
+    name: document.getElementById("memberName").value,
+    initiative: document.getElementById("memberInitiative").value,
+  };
+
   membersBlock.insertAdjacentHTML(
     "afterbegin",
     `
@@ -20,7 +28,8 @@ function addMember() {
       </div>
       <button
         id="deleteButton"
-        class="text-red-700 deleteButton" type="button"
+        class="text-red-700 deleteButton"
+        type="button"
       >
         X
       </button>
@@ -28,17 +37,47 @@ function addMember() {
   `
   );
 
+  deleteButtons[memberIndex] = {
+    index: memberIndex,
+    deleteButton: document.getElementById("deleteButton"),
+  };
+  memberIndex++;
+  // console.log(members);
+  // console.log(deleteButtons);
+
   /*delete-member*/
-  deleteButtons.push(document.getElementById("deleteButton"));
-  deleteButtons.forEach((deleteButton, index) => [
-    deleteButton.addEventListener("click", function () {
-      deleteButton.parentNode.parentNode.removeChild(deleteButton.parentNode);
-      console.log("index: ", index);
-      deleteButtons.splice(index, 1);
-      members.splice(index, 1);
-      console.log(members);
+  Object.keys(deleteButtons).forEach((key) => [
+    deleteButtons[key].deleteButton.addEventListener("click", function () {
+      deleteButtons[key].deleteButton.parentNode.parentNode.removeChild(
+        deleteButtons[key].deleteButton.parentNode
+      );
+      delete deleteButtons[key];
+      delete members[key];
+      console.log(key, "deleted");
+      console.log("members", members);
+      console.log(deleteButtons);
     }),
+    // deleteButton.addEventListener("click", function () {
+    //   console.log("deleted");
+    // }),
   ]);
+
+  // deleteButtons.forEach((deleteButton, index) => [
+  //   deleteButton.addEventListener("click", function () {
+  //     deleteButton.parentNode.parentNode.removeChild(deleteButton.parentNode);
+  //     // console.log("index: ", index, "; member del");
+  //     // deleteButtons.splice(index, 1);
+  //     console.log(members[index], " deleted");
+  //     delete members[index];
+  //     console.log(members);
+  //     // deleteButtons.forEach((_deleteButton, index) => {
+  //     //   // console.log("index: ", index, "; member del");
+  //     //   // deleteButtons.splice(index, 1);
+  //     //   // members.splice(index, 1);
+  //     //   // console.log(members);
+  //     // });
+  //   }),
+  // ]);
 }
 
 /*checkbox*/
