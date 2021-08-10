439c8537716edcf13f276372019e0a6cbd82b8ed
DmytroPasenko <elengardway@gmail.com>
2021-08-10 18:55:35 +0300

    The logic for sorting the battle queue has been written. Added clearing the list of battle members

--- js/script.js
+++ js/script.js
@@ -12,16 +12,15 @@ function temp() {
 /*temp-function end*/
 
 function buttonClassToggle(button, condition) {
-  if (Object.keys(members).length === 0 && members.constructor === Object) {
-    button.classList.toggle("blockedButton");
-    button.classList.toggle("commonButton");
-    button.disabled = condition;
-    console.log(button.disabled, condition);
-  }
+  button.classList.toggle("blockedButton");
+  button.classList.toggle("commonButton");
+  button.disabled = condition;
 }
 
 function addMember() {
-  buttonClassToggle(generateButton, false);
+  if (Object.keys(members).length === 0 && members.constructor === Object) {
+    buttonClassToggle(generateButton, false);
+  }
   members[memberIndex] = {
     index: memberIndex,
     name: document.getElementById("memberName").value,
@@ -45,7 +44,7 @@ function addMember() {
         X
       </button>
     </div>
-  `
+    `
   );
 
   deleteButtons[memberIndex] = {
@@ -62,9 +61,86 @@ function addMember() {
       );
       delete deleteButtons[key];
       delete members[key];
-      buttonClassToggle(generateButton, true);
+      if (Object.keys(members).length === 0 && members.constructor === Object) {
+        buttonClassToggle(generateButton, true);
+      }
     }),
   ]);
+}
+
+/*battle-generation*/
+const nextMemberButton = document.getElementById("nextMemberButton"),
+  addMemberButton = document.getElementById("addMemberButton"),
+  currentMember = document.getElementById("currentMember"),
+  table = document.getElementById("table");
+let sortedMembers = {},
+  sortedMembersArray = [];
+
+function battleGeneration() {
+  buttonClassToggle(nextMemberButton, true);
+
+  Object.keys(members).forEach((key) => {
+    sortedMembers[key] = {
+      name: members[key].name,
+      initiative: members[key].initiative,
+    };
+  });
+
+  console.log(
+    Object.values(sortedMembers).sort((a, b) =>
+      a.initiative > b.initiative ? 1 : -1
+    )
+  );
+
+  sortedMembersArray = Object.values(sortedMembers).sort((a, b) =>
+    a.initiative > b.initiative ? 1 : -1
+  );
+
+  /*filling-up-the-battle-queue*/
+  sortedMembersArray.forEach((member) => [
+    table.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <div class="grid grid-cols-2 items-center mx-auto my-2">
+        <div class="flex normal-case">
+          <p>${member.name}</p>
+          <span class="mx-2">—</span>
+          <p>${member.initiative}</p>
+        </div>
+        <select
+          class="bg-transparent ml-3 memberStatusSelector"
+          name="status"
+          id="memberStatus"
+        >
+          <option value="0">Alive</option>
+          <option value="1">Unconscious</option>
+          <option value="2">Death</option>
+        </select>
+      </div>
+      `
+    ),
+  ]);
+
+  clearMembersList();
+}
+
+function clearMembersList() {
+  members = {};
+  deleteButtons = {};
+  while (membersBlock.hasChildNodes()) {
+    membersBlock.removeChild(membersBlock.firstChild);
+  }
+  if (!membersBlock.hasChildNodes()) {
+    membersBlock.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <p class="text-center col-span-2 uppercase mt-2">All members in a battle!</p>
+      `
+    );
+  }
+  buttonClassToggle(addMemberButton, true);
+  currentMember.innerText =
+    sortedMembersArray[sortedMembersArray.length - 1].name;
 }
 
 /*checkbox*/
